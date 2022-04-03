bb897f03fd37c9541d23c4361eac81d0cd9e71c9
Dmitriy <dimakim2001@mail.ru>
2022-04-03 11:06:56 +0300

    added array

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] array = {"1", "22", "333", "4444", "55555", "66"};
+
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
