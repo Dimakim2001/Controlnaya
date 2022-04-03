66d748623a78af5cf7962b0ed2c5f78d092d4271
Dimakim2001 <97974917+Dimakim2001@users.noreply.github.com>
2022-04-03 14:38:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] array = {"1", "22", "333", "4444", "55555", "66"};
+
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
